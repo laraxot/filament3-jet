0179c242550ff6f6056ee1a5e01aa1cf28cd9375
Davide Cavallini <davidecavallini1987@gmail.com>
2023-07-04 10:31:29 +0200

    .

--- stubs/app/Actions/FilamentJet/CreateTeam.php
+++ stubs/app/Actions/FilamentJet/CreateTeam.php
@@ -7,12 +7,15 @@ namespace App\Actions\FilamentJet;
 <<<<<<< HEAD
 =======
 use App\Models\Team;
+<<<<<<< HEAD
 >>>>>>> eee9f5f (Fix styling)
 =======
 use Modules\User\Models\Team;
 >>>>>>> 798d2d5 (.)
 =======
 >>>>>>> 88c140b (Fix styling)
+=======
+>>>>>>> eee9f5f0 (Fix styling)
 use ArtMin96\FilamentJet\Contracts\CreatesTeams;
 use ArtMin96\FilamentJet\Events\AddingTeam;
 use ArtMin96\FilamentJet\FilamentJet;
